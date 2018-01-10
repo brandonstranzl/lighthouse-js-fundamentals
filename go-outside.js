11787a5bc8ebece7e5a331c3ab5d59c0bc7c1540
Brandon Stranzl <brandonstranzl@gmail.com>
2018-01-10 19:04:17 +0000

    Adding these files which are my blotter tests of the material - howManyHundreds, and go-outside.

---
+++ go-outside.js
@@ -0,0 +1,32 @@
+//Raining
+var raining = false;
+var cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Temperature
+var temperature = 25;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Test of the ! operator
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
---
+++ howManyHundreds.js
@@ -0,0 +1,15 @@
+function howManyHundreds(number) {
+  return (number/100) - (number%100)/100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
+
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
